8432a84cc116e9a38dd5341fe905e08137390231
Jonathan.Tejero <jonathan.tejero@flywire.com>
2024-09-19 12:30:39 +0200

    Run e2e in pipeline

--- src/test/java/com/example/runners/CucumberTestRunner.java
+++ src/test/java/com/example/runners/CucumberTestRunner.java
@@ -1,16 +1,23 @@
 package com.example.runners;
 
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+
+
 @CucumberOptions(
-        features = "src/test/resources/features", // Ruta de los archivos .feature
-        glue = {"com/example/steps"}, // Paquete de las step definitions
-        plugin = {"pretty", "html:target/cucumber-reports.html"}, // Opciones de salida
-        monochrome = true // Formato limpio en la consola
+        features = "src/test/resources/features",
+        glue = {"com/example/steps"},
+        plugin = {"pretty", "html:target/cucumber-reports.html"},
+        monochrome = true
 )
-public class CucumberTestRunner {
+public class CucumberTestRunner  extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
 
