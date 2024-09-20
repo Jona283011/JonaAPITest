59b8931a6a40738b138a5a93a23f66b8829b272c
Jonathan.Tejero <jonathan.tejero@flywire.com>
2024-09-20 09:17:16 +0200

    Last UserController optimization

--- src/main/java/com/example/model/controllers/UserController.java
+++ src/main/java/com/example/model/controllers/UserController.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
 
 import java.io.FileInputStream;
@@ -20,114 +21,84 @@ public class UserController {
     private final String BaseUrl = "https://api.github.com/";
     ObjectMapper objectMapper = new ObjectMapper();
 
+    public UserController() {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
     public PrivateUser getUserEndpoint(String githubToken) throws JsonProcessingException {
         String url = BaseUrl + "user";
-
-        Response response = RestAssured
-                .given()
-                .baseUri(url)
-                .header("Authorization", "Bearer " +getTokenFromProperties(githubToken))
-                .when()
-                .get()
-                .then()
-                .extract()
-                .response();
-
-        String responseBody = response.getBody().asString();
-        assertEquals(response.statusCode(), HttpStatus.SC_OK, "StatusCode should be:"+ HttpStatus.SC_OK);
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper.readValue(responseBody, PrivateUser.class);
+        Response response = sendGetRequest(url, githubToken);
+        validateResponse(response, HttpStatus.SC_OK, null);
+        return deserializeResponse(response, HttpStatus.SC_OK);
     }
 
     public PrivateUser getUserEndpoint(String githubToken, int expectedStatusCode) throws JsonProcessingException {
         String url = BaseUrl + "user";
-
-        Response response = RestAssured
-                .given()
-                .baseUri(url)
-                .header("Authorization", "Bearer " +getTokenFromProperties(githubToken))
-                .when()
-                .get()
-                .then()
-                .extract()
-                .response();
-
-        String responseBody = response.getBody().asString();
-        assertEquals(response.statusCode(), expectedStatusCode, "StatusCode should be: "+ expectedStatusCode);
-        objectMapper.registerModule(new JavaTimeModule());
-        if(expectedStatusCode != HttpStatus.SC_OK) {
-            return null;
-        }else{
-            return objectMapper.readValue(responseBody, PrivateUser.class);
-        }
+        Response response = sendGetRequest(url, githubToken);
+        validateResponse(response, expectedStatusCode, null);
+        return deserializeResponse(response, expectedStatusCode);
     }
 
     public PrivateUser getUserByUsernameEndpoint(int expectedStatusCode, String expectedMessage, String userName) throws JsonProcessingException {
         String url = BaseUrl + "users/" + userName;
-
-        Response response = RestAssured
-                .given()
-                .baseUri(url)
-                .when()
-                .get()
-                .then()
-                .extract()
-                .response();
-
-        String responseBody = response.getBody().asString();
-        assertEquals(response.statusCode(), expectedStatusCode, "StatusCode should be:"+ expectedStatusCode);
-        assertTrue(response.statusLine().contains(expectedMessage), "Response body should contain: "+expectedMessage);
-        objectMapper.registerModule(new JavaTimeModule());
-        if(expectedStatusCode != HttpStatus.SC_OK) {
-            return null;
-        }else{
-            return objectMapper.readValue(responseBody, PrivateUser.class);
-        }
+        Response response = sendGetRequest(url, null);
+        validateResponse(response, expectedStatusCode, expectedMessage);
+        return deserializeResponse(response, expectedStatusCode);
     }
 
     public PrivateUser patchUserEndpoint(String githubToken, String requestBody) throws JsonProcessingException {
         String url = BaseUrl + "user";
-
-        Response response = RestAssured
-                .given()
-                .baseUri(url)
-                .header("Authorization", "Bearer " +getTokenFromProperties(githubToken))
-                .body(requestBody)
-                .when()
-                .patch()
-                .then()
-                .extract()
-                .response();
-
-        String responseBody = response.getBody().asString();
-        assertEquals(response.statusCode(), HttpStatus.SC_OK, "StatusCode should be:"+ HttpStatus.SC_OK);
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper.readValue(responseBody, PrivateUser.class);
+        Response response = sendPatchRequest(url, githubToken, requestBody);
+        validateResponse(response, HttpStatus.SC_OK);
+        return deserializeResponse(response, HttpStatus.SC_OK);
     }
 
     public PrivateUser patchUserEndpoint(String githubToken, String requestBody, int expectedStatusCode) throws JsonProcessingException {
         String url = BaseUrl + "user";
+        Response response = sendPatchRequest(url, githubToken, requestBody);
+        validateResponse(response, expectedStatusCode);
+        return deserializeResponse(response, expectedStatusCode);
+    }
 
-        Response response = RestAssured
+    private Response sendGetRequest(String url, String githubToken) {
+        RequestSpecification request = RestAssured.given().baseUri(url);
+        if (githubToken != null && !githubToken.isEmpty()) {
+            request.header("Authorization", "Bearer " + getTokenFromProperties(githubToken));
+        }
+        return request.when().get().then().extract().response();
+    }
+
+    private void validateResponse(Response response, int expectedStatusCode, String expectedMessage) {
+        assertEquals(response.statusCode(), expectedStatusCode, "StatusCode should be: " + expectedStatusCode);
+        if (expectedMessage != null) {
+            assertTrue(response.statusLine().contains(expectedMessage), "Response should contain: " + expectedMessage);
+        }
+    }
+
+    private void validateResponse(Response response, int expectedStatusCode) {
+        assertEquals(response.statusCode(), expectedStatusCode, "StatusCode should be: " + expectedStatusCode);
+    }
+
+    private PrivateUser deserializeResponse(Response response, int expectedStatusCode) throws JsonProcessingException {
+        if (expectedStatusCode != HttpStatus.SC_OK) {
+            return null;
+        }
+        return objectMapper.readValue(response.getBody().asString(), PrivateUser.class);
+    }
+
+    private Response sendPatchRequest(String url, String githubToken, String requestBody) {
+        return RestAssured
                 .given()
                 .baseUri(url)
-                .header("Authorization", "Bearer " +getTokenFromProperties(githubToken))
+                .header("Authorization", "Bearer " + getTokenFromProperties(githubToken))
                 .body(requestBody)
                 .when()
                 .patch()
                 .then()
                 .extract()
                 .response();
-
-        String responseBody = response.getBody().asString();
-        assertEquals(response.statusCode(), expectedStatusCode, "StatusCode should be:"+ expectedStatusCode);
-        objectMapper.registerModule(new JavaTimeModule());
-        if(expectedStatusCode != HttpStatus.SC_OK) {
-            return null;
-        }else{
-            return objectMapper.readValue(responseBody, PrivateUser.class);
-        }
     }
+
 
     private static String getTokenFromProperties(String githubToken) {
         Properties properties = new Properties();
